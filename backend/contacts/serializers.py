bddd20e79cdce533e29a603896b3aca2ea6582db
Tuzmore <tuzmorecrm@gmail.com>
2025-08-03 22:45:01 +0300

    Added  ContactSerializer for API conversion

---
+++ backend/contacts/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Contact
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        read_only_fields = ['user', 'created_at']
