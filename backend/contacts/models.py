168bfa839b2754f2901a4d990f44ffa229908747
Tuzmore <tuzmorecrm@gmail.com>
2025-08-03 22:39:14 +0300

    Add Contact model with fields for name, email, phone , company

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Contact(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField()
+    phone = models.CharField(max_length=20)
+    company = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
