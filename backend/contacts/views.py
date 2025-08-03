4f5097dd8041669d6bb7081be5f8a6fe34006cc0
Tuzmore <tuzmorecrm@gmail.com>
2025-08-03 22:57:28 +0300

    Added ContactListCreateView and ContactDetailView with auth filtering

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .models import Contact
+from rest_framework import generics, permissions
+from .serializers import ContactSerializer
 # Create your views here.
+
+class ContactListCreateView(generics.ListCreateAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return
+    Contact.objects.filter(user=self.request.user)
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
+class ContactRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return
+    Contact.objects.filter(user=self.request.user)
