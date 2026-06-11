84c3ebb2b21ee8140637e982f149ad67a300287e
Tuzmore <tuzmorecrm@gmail.com>
2026-06-11 14:12:29 +0300

    Create accounts app custom user model

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
+
