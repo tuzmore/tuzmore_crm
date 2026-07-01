0ad40ba3e3441b6bbe02c990c41889125ffa5d02
Tuzmore <tuzmorecrm@gmail.com>
2026-07-01 14:49:44 +0300

    Complete contacts CRUD

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
+from .views import(
+    contact_list,
+    contact_create,
+    contact_detail,
+    contact_update,
+    contact_delete
+)
 
 urlpatterns = [
-    
+    path("", contact_list, name="contact_list"),
+    path("create/", contact_create, name="contact_create"),
+    path("<int:pk>/", contact_detail, name="contact_detail"),
+    path("<int:pk>/edit/", contact_update, name="contact_update"),
+    path("<int:pk>/delet/", contact_delete, name="contact_delete"),
+
 ]
